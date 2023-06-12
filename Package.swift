c146e0547ea4a1fc8a025f5e82b5fcb5e52480ee
pulsely <opensource@pulsely.com>
2023-06-12 14:35:06 +0100

    Update release 0.1.6

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftNFCClosureHandler",
-            dependencies: []),
-    ]
+            dependencies: [], path: "Sources",
+            linkerSettings: [.linkedFramework("CoreNFC", .when(platforms: [.iOS]))]),
+    ], swiftLanguageVersions: [.v5]
 )
